b42ab2dc4eedddecd71011be0d5291fd1b3e9be0
eminshamshoian <eminshamshoian@Emins-MacBook-Pro.local>
2020-05-02 01:12:39 -0700

    components, states & props

---
+++ src/components/users/Search.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+export class Search extends Component {
+    render() {
+        return (
+            <div>
+                <form className=''>
+                    <input
+                        type='text'
+                        name='text'
+                        placeholder='Search Users...'
+                    />
+                    <input
+                        type='submit'
+                        value='Search'
+                        className='btn btn-dark btn-block'
+                    />
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Search;
